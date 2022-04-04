330fb7d2289b893c0f4380ceabc68743a25e9c21
Matheus Grossi <86444366+matheusgrossi7@users.noreply.github.com>
2022-04-04 01:24:41 -0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -17,8 +17,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b80df7e260d2780d43b0de31d2730d0f",
-"/": "b80df7e260d2780d43b0de31d2730d0f",
+"index.html": "679a8f57d042f30f3d51e50a940f324d",
+"/": "679a8f57d042f30f3d51e50a940f324d",
 "main.dart.js": "9b38ec5bf9c9bd94c57b7e44b8410e77",
 "manifest.json": "5ec942eea7a5633cd21808ba978df597",
 "version.json": "5fecd3a0dd6156598408115becb2e8a6"
